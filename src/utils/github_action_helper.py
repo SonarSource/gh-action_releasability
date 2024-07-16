7337bd676c05100a437ed7d7a7bdb772a8580a6d
Sebastien Vermeille <sebastien.vermeille@sonarsource.com>
2024-07-16 13:19:30 +0200

    BUILD-5702 Add github-action-utils lib to ease usage of GitHub workflows/actions toolkit

--- src/utils/github_action_helper.py
+++ src/utils/github_action_helper.py
@@ -1,24 +1,14 @@
 import os
 import uuid
 
+from github_action_utils import set_output
+
 GITHUB_ACTION_OUTPUT_LOG_NAME = "logs"
 GITHUB_ACTION_OUTPUT_STATUS_NAME = "status"
 GITHUB_OUTPUT_ENVIRONMENT_NAME = "GITHUB_OUTPUT"
 
 
 class GithubActionHelper:
-
-    @staticmethod
-    def set_output(output_name, value):
-        """Sets the GitHub Action output.
-
-        Keyword arguments:
-        output_name - The name of the output
-        value - The value of the output
-        """
-        if GITHUB_OUTPUT_ENVIRONMENT_NAME in os.environ:
-            with open(os.environ[GITHUB_OUTPUT_ENVIRONMENT_NAME], "a") as output_stream:
-                print(f"{output_name}={value}", file=output_stream)
 
     @staticmethod
     def set_multiline_output(output_name, value):
@@ -41,4 +31,4 @@ class GithubActionHelper:
 
     @staticmethod
     def set_output_status(status):
-        GithubActionHelper.set_output(GITHUB_ACTION_OUTPUT_STATUS_NAME, status)
+        set_output(GITHUB_ACTION_OUTPUT_STATUS_NAME, status)
--- tests/utils/github_action_helper_test.py
+++ tests/utils/github_action_helper_test.py
@@ -6,13 +6,6 @@ from utils.github_action_helper import GithubActionHelper
 
 
 class GithubActionHelperTest(unittest.TestCase):
-    def test_set_output(self):
-        with tempfile.NamedTemporaryFile(suffix="", prefix=os.path.basename(__file__)) as temp_file:
-            os.environ['GITHUB_OUTPUT'] = temp_file.name
-
-            GithubActionHelper.set_output('function', 'output')
-
-            assert temp_file.read().decode("utf-8").strip() == "function=output"
 
     def test_set_multiline_output(self):
         with tempfile.NamedTemporaryFile(suffix="", prefix=os.path.basename(__file__)) as temp_file:
